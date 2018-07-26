4af7b560fb5890436f57fef4aef57e695d456974
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2018-07-26 21:18:58 +0530

    Add Gcloud SDK - fixes #1

--- scripts/post-install-juno.sh
+++ scripts/post-install-juno.sh
@@ -7,8 +7,15 @@ export DEBIAN_FRONTEND=noninteractive
 echo Doing post-install steps for Juno...
 echo -----------------------------------------------------------------------------
 
+# Gcloud initialization
+export CLOUD_SDK_REPO="cloud-sdk-bionic"
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+apt-get -qq update
+
 required_packages="haveged \
-    ethtool"
+    ethtool \
+    google-cloud-sdk"
 
 for package in $required_packages
 do
